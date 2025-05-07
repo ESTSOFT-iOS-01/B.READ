35b247b6c3842a93856f3e0696dc867531f62658
shinseungjae <mok05247@naver.com>
2025-05-07 15:27:34 +0900

    초기 프로젝트 임포트

---
+++ B.READ/B.READ/B_READApp.swift
@@ -0,0 +1,17 @@
+//
+//  B_READApp.swift
+//  B.READ
+//
+//  Created by 신승재 on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct B_READApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ B.READ/B.READ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  B.READ
+//
+//  Created by 신승재 on 5/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
