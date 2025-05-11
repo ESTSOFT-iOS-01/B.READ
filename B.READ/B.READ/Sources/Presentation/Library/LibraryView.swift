86f7a0208ccd5dc621dd13a3e51b3cf3d7f2195b
shinseungjae <mok05247@naver.com>
2025-05-11 16:24:37 +0900

    뷰 디렉터리 나누기

---
+++ B.READ/B.READ/Sources/Presentation/Library/LibraryView.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryView.swift
+//  B.READ
+//
+//  Created by 신승재 on 5/11/25.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LibraryView()
+}
---
+++ B.READ/B.READ/Sources/Presentation/MyPage/MyPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPageView.swift
+//  B.READ
+//
+//  Created by 신승재 on 5/11/25.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPageView()
+}
