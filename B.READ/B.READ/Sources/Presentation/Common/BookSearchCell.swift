3ff16ab781863787c202d6c0a79359157fae3098
shinseungjae <mok05247@naver.com>
2025-05-14 13:27:22 +0900

    [#26] BookSearchCell 구현

--- B.READ/B.READ/Sources/Presentation/Common/BookSearchCell.swift
+++ B.READ/B.READ/Sources/Presentation/Common/BookSearchCell.swift
@@ -8,22 +8,35 @@
 import SwiftUI
 
 struct BookSearchCell: View {
+  
+  let coverImage: Image
+  let title: String
+  let author: String
+  let publisher: String
+  let publishedDate: Date
+  
   var body: some View {
-    HStack(spacing: 24) {
-      Image(.exampleBook)
+    HStack(alignment: .top, spacing: 24) {
+      coverImage
         .resizable()
         .aspectRatio(contentMode: .fill)
         .frame(width: 58 ,height: 88)
         .clipShape(RoundedRectangle(cornerRadius: 6))
+        .shadow(color: .black.opacity(0.25), radius: 25, x: 0, y: 2)
       
-      VStack(alignment: .leading, spacing: 3) {
-        Text("데미안")
+      VStack(alignment: .leading, spacing: 8) {
+        Text(title)
+          .foregroundStyle(.black) // TODO: GrayScale로 변경해야함
           .brStyleFont(.pretendard(.semiBold, size: 18), lineHeight: 1.0)
-        Text("헤르만 헤세")
+        Text(author)
+          .foregroundStyle(.brown8)
           .brStyleFont(.pretendard(.medium, size: 14), lineHeight: 1.0)
-        Text("민음사 | 2009.01.02")
+        Text("\(publisher) | \(publishedDate.string(format: .dotSeparated))")
+          .foregroundStyle(.brown5)
           .brStyleFont(.pretendard(.regular, size: 12), lineHeight: 1.0)
-      }.frame(maxWidth: .infinity, alignment: .leading)
+      }
+      .frame(maxWidth: .infinity, alignment: .leading)
+      .padding(.top, 4)
       
     }
     .frame(maxWidth: .infinity)
@@ -32,6 +45,11 @@ struct BookSearchCell: View {
 }
 
 #Preview {
-  BookSearchCell()
-    .background(.gray)
+  BookSearchCell(
+    coverImage: Image(.exampleBook),
+    title: "데미안",
+    author: "헤르만 헤세",
+    publisher: "민음사",
+    publishedDate: Date()
+  )
 }
