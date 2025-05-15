74e86387455b7cf7a7551d2caadff44f171ddcdd
shinseungjae <mok05247@naver.com>
2025-05-15 10:30:08 +0900

    [#30] OnBoardingStep enum 추가

---
+++ B.READ/B.READ/Sources/Presentation/OnBoarding/OnBoardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnBoardingView.swift
+//  B.READ
+//
+//  Created by 신승재 on 5/14/25.
+//
+
+import SwiftUI
+
+
+
+struct OnBoardingView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+#Preview {
+  OnBoardingView()
+}
