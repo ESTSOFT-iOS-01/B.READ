1e60be7ef3f0dfecff17547df5fcf9bd7b176ee3
심근웅 <geunwoong.sim@gmail.com>
2025-05-15 22:37:07 +0900

    [#38] LibraryView 상단 탭바 구현중, LibraryViewModel 생성

--- B.READ/B.READ/Sources/Presentation/Library/LibraryView.swift
+++ B.READ/B.READ/Sources/Presentation/Library/LibraryView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct LibraryView: View {
+  let tabs = [
+    TabItem(title: "전체"),
+    TabItem(title: "읽은 책"),
+    TabItem(title: "읽는 중"),
+    TabItem(title: "전체"),
+    TabItem(title: "전체"),
+  ]
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
