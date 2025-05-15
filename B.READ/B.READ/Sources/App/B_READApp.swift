e6246be02a307e98fb8e2bdbcb74c7bee764b0e9
shinseungjae <mok05247@naver.com>
2025-05-15 20:59:27 +0900

    [#34] NicknameView 구현

--- B.READ/B.READ/Sources/App/B_READApp.swift
+++ B.READ/B.READ/Sources/App/B_READApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct B_READApp: App {
   var body: some Scene {
     WindowGroup {
-      MainTabView()
+      NicknameView()
     }
   }
 }
--- B.READ/B.READ/Sources/Presentation/Common/NicknameView.swift
+++ B.READ/B.READ/Sources/Presentation/Common/NicknameView.swift
@@ -8,10 +8,35 @@
 import SwiftUI
 
 struct NicknameView: View {
+  
+  @FocusState private var isFocused: Bool
+  @State private var nickname = ""
+  
   var body: some View {
-    VStack {
+    VStack(alignment: .leading, spacing: 0) {
       Text("사용할 닉네임을 입력해주세요.")
+        .brStyleFont(.pretendard(.semiBold, size: 24), lineHeight: 1.4)
+        .padding(.top, 24)
+        .padding(.leading, 6)
+      
       Text("영어, 한글, 숫자로 최대 12자까지 설정할 수 있습니다.")
+        .brStyleFont(.pretendard(.regular, size: 14), lineHeight: 1.35)
+        .padding(.leading, 6)
+      
+      TextField("닉네임을 입력해주세요.", text: $nickname)
+        .padding(.top, 40)
+        .focused($isFocused)
+      
+      BottomButton(buttonTitle: "확인", textColor: .gray3, buttonColor: .gray0) {
+        print("next")
+      }
+      .padding(.horizontal, 4)
+      .padding(.bottom, 20)
+      .frame(maxHeight: .infinity, alignment: .bottom)
+    }
+    .padding(.horizontal, 26)
+    .onAppear {
+      isFocused = true
     }
   }
 }
