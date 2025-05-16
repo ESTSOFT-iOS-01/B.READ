53a2dbb0c4c697ba1a4f0c099106f45a45b39df7
shinseungjae <mok05247@naver.com>
2025-05-16 10:50:28 +0900

    [#34] CategorySelectionView 구현

--- B.READ/B.READ/Sources/Presentation/Common/CategorySelectionView.swift
+++ B.READ/B.READ/Sources/Presentation/Common/CategorySelectionView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct CategorySelectionView: View {
   
-  private var isButtonEnabled = false
+  @State private var selectedCategories: Set<Category> = []
+  private var isButtonEnabled: Bool {
+    selectedCategories.count == 2
+  }
   
   var body: some View {
     VStack(alignment: .leading, spacing: 0) {
@@ -24,7 +27,7 @@ struct CategorySelectionView: View {
         .brStyleFont(.pretendard(.regular, size: 14), lineHeight: 1.35)
         .padding(.leading, 6)
       
-      CategoryListView()
+      CategoryListView(selectedCategories: $selectedCategories)
         .padding(.top, 24)
       
       BottomButton(
@@ -35,6 +38,7 @@ struct CategorySelectionView: View {
         print("next")
       }
       .disabled(!isButtonEnabled)
+      .animation(.easeInOut(duration: 0.25), value: isButtonEnabled)
       .padding(.horizontal, 6)
       .padding(.top, 12)
       
@@ -45,27 +49,51 @@ struct CategorySelectionView: View {
 
 // MARK: - (S)CategoryListView
 private struct CategoryListView: View {
+  
+  @Binding var selectedCategories: Set<Category>
+  
   var body: some View {
     ScrollView {
       VStack(alignment: .leading, spacing: 16) {
-        ForEach(Category.allCases, id: \.self) { category in
-          Button {
-            print("select\(category)")
-          } label: {
-            HStack(spacing: 16) {
-              Image(systemName: "checkmark")
-                .resizable()
-                .frame(width: 14, height: 11)
-                .foregroundStyle(.gray1)
-              Text(category.displayName)
-                .foregroundStyle(.black)
-                .brStyleFont(.pretendard(.medium, size: 18), lineHeight: 1.35, letterSpacing: 0.02)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            }
-          }
+        ForEach(Category.allCases, id: \.self) {
+          SelectionRow(selectedCategories: $selectedCategories, category: $0)
         }
       }
     }.scrollIndicators(.never)
+  }
+}
+
+
+// MARK: - (S)SelectionRow
+private struct SelectionRow: View {
+  
+  @Binding var selectedCategories: Set<Category>
+  let category: Category
+  private var isSelected: Bool {
+    selectedCategories.contains(category)
+  }
+  
+  var body: some View {
+    Button {
+      switch isSelected {
+      case false:
+        if selectedCategories.count < 2 { selectedCategories.insert(category) }
+      case true:
+        if selectedCategories.contains(category) { selectedCategories.remove(category) }
+      }
+    } label: {
+      HStack(spacing: 16) {
+        Image(systemName: "checkmark")
+          .resizable()
+          .frame(width: 14, height: 11)
+          .foregroundStyle(isSelected ? .brown8 : .gray1)
+          .bold(isSelected)
+        Text(category.displayName)
+          .foregroundStyle(.black)
+          .brStyleFont(.pretendard(.medium, size: 18), lineHeight: 1.35, letterSpacing: 0.02)
+          .frame(maxWidth: .infinity, alignment: .leading)
+      }
+    }
   }
 }
 
