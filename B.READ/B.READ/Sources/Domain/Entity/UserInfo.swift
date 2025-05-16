4ff47715e31cfe8b35fd6cb1f08bbab38a4f99d4
shinseungjae <mok05247@naver.com>
2025-05-16 15:06:20 +0900

    [#45] UserInfo Entity 수정

--- B.READ/B.READ/Sources/Domain/Entity/UserInfo.swift
+++ B.READ/B.READ/Sources/Domain/Entity/UserInfo.swift
@@ -10,15 +10,17 @@ import Foundation
 /// 기본 유저 정보입니다.
 /// - nickname : 유저 닉네임(영어, 한글, 숫자, 12자 이내)
 /// - categories : 선호장르 - [Category]
-/// - recentKeywords : 최근 검색어 - [String]
+/// - recentKeywords : 최근 검색어 - [Keyword]
 /// - generateCount : ai 요약 생성횟수
-/// - streak : 일주일간의 독서 유무 - [Bool]
+/// - lastStreakUpdatedAt: 스트릭 업데이트 날짜
+/// - streak : 일주일간의 독서 유무 - [DailyStatus]
 struct UserInfo {
   var nickname: String
   var categories: [Category]
-  var recentKeywords: [String]
+  var recentKeywords: [Keyword]
   var generateCount: Int
-  var streak: [Bool]
+  var lastStreakUpdatedAt: Date
+  var streak: [DailyStatus]
 }
 
 /// 책 장르입니다.
@@ -28,4 +30,21 @@ struct UserInfo {
 struct Category {
   let id: Int
   let name: String
+}
+
+/// 최근 검색 키워드입니다.
+/// - date: 키워드를 검색한 날짜
+/// - value: 검색한 키워드 문자열
+struct Keyword {
+  let date: Date
+  let value: String
+}
+
+
+/// 요일별 독서 스트릭의 하루별 상태입니다..
+/// - weekday: 요일 인덱스 (0 = 일요일 ~ 6 = 토요일)
+/// - isCompleted: 해당 요일에 독서를 메모, 문장작성을 완료 했는지 여부
+struct DailyStatus {
+  let weekday: Int
+  var isCompleted: Bool
 }
