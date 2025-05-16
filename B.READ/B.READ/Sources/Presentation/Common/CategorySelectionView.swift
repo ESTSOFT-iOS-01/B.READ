faeb95c4e7b7a9713fec3e33d4fc7e74e9c39a30
shinseungjae <mok05247@naver.com>
2025-05-16 09:25:51 +0900

    [#34] CategoryListView 추가

--- B.READ/B.READ/Sources/Presentation/Common/CategorySelectionView.swift
+++ B.READ/B.READ/Sources/Presentation/Common/CategorySelectionView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct CategorySelectionView: View {
+  
+  private var isButtonEnabled = false
+  
   var body: some View {
     VStack(alignment: .leading, spacing: 0) {
       Text("관심 분야를 선택해 주세요.")
@@ -20,7 +23,49 @@ struct CategorySelectionView: View {
         .foregroundStyle(.gray5)
         .brStyleFont(.pretendard(.regular, size: 14), lineHeight: 1.35)
         .padding(.leading, 6)
+      
+      CategoryListView()
+        .padding(.top, 24)
+      
+      BottomButton(
+        buttonTitle: "저장하기",
+        textColor: isButtonEnabled ?  .backgroundDefault : .gray3,
+        buttonColor: isButtonEnabled ? .brown3 : .gray0
+      ) {
+        print("next")
+      }
+      .disabled(!isButtonEnabled)
+      .padding(.horizontal, 6)
+      .padding(.top, 12)
+      
     }
+    .padding(.horizontal, 24)
+  }
+}
+
+// MARK: - (S)CategoryListView
+private struct CategoryListView: View {
+  var body: some View {
+    ScrollView {
+      VStack(alignment: .leading, spacing: 16) {
+        ForEach(Category.allCases, id: \.self) { category in
+          Button {
+            print("select\(category)")
+          } label: {
+            HStack(spacing: 16) {
+              Image(systemName: "checkmark")
+                .resizable()
+                .frame(width: 14, height: 11)
+                .foregroundStyle(.gray1)
+              Text(category.displayName)
+                .foregroundStyle(.black)
+                .brStyleFont(.pretendard(.medium, size: 18), lineHeight: 1.35, letterSpacing: 0.02)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+          }
+        }
+      }
+    }.scrollIndicators(.never)
   }
 }
 
