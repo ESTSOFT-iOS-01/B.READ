386dae2568e99b1a9be1f30f4a97fd7ec303d230
shinseungjae <mok05247@naver.com>
2025-05-17 11:05:43 +0900

    [#45] 테스트 코드 작성

--- B.READ/B.READ/Sources/Domain/Entity/UserInfo.swift
+++ B.READ/B.READ/Sources/Domain/Entity/UserInfo.swift
@@ -14,7 +14,7 @@ import Foundation
 /// - generateCount : ai 요약 생성횟수
 /// - lastStreakUpdatedAt: 스트릭 업데이트 날짜
 /// - streak : 일주일간의 독서 유무 - [DailyStatus]
-struct UserInfo {
+struct UserInfo: Equatable {
   var nickname: String
   var categories: [Category]
   var recentKeywords: [Keyword]
@@ -27,7 +27,7 @@ struct UserInfo {
 /// 선호 도서 추천에서 사용됩니다.
 /// - id : 알라딘 API의 장르 CID
 /// - name : 장르명
-struct Category {
+struct Category: Equatable {
   let id: Int
   let name: String
 }
@@ -35,7 +35,7 @@ struct Category {
 /// 최근 검색 키워드입니다.
 /// - date: 키워드를 검색한 날짜
 /// - value: 검색한 키워드 문자열
-struct Keyword {
+struct Keyword: Equatable {
   let date: Date
   let value: String
 }
@@ -44,7 +44,7 @@ struct Keyword {
 /// 요일별 독서 스트릭의 하루별 상태입니다..
 /// - weekday: 요일 인덱스 (0 = 일요일 ~ 6 = 토요일)
 /// - isCompleted: 해당 요일에 독서를 메모, 문장작성을 완료 했는지 여부
-struct DailyStatus {
+struct DailyStatus: Equatable {
   let weekday: Int
   var isCompleted: Bool
 }
---
+++ B.READ/B.READTests/UserInfoRepositoryTest/UserInfoRepositoryTests.swift
@@ -0,0 +1,91 @@
+//
+//  UserInfoRepositoryTests.swift
+//  B.READTests
+//
+//  Created by 신승재 on 5/17/25.
+//
+
+import Foundation
+import Testing
+
+struct UserInfoRepositoryTests {
+  
+  private let userInfoRepository: UserInfoRepository
+  
+  init() {
+    userInfoRepository = UserInfoRepositoryStub()
+  }
+  
+
+  @Test("UserInfo Create Test")
+  func createUserInfo() async throws {
+    
+    try await userInfoRepository.createUserInfo(DummyData.userInfo)
+    let fetchedUserInfo = try await userInfoRepository.fetchUserInfo()
+    
+    #expect(fetchedUserInfo == DummyData.userInfo)
+  }
+  
+  @Test("UserInfo Create Error Test - Data Already Exists")
+  func createUserInfoDataAlreadyExist() async throws {
+    try await userInfoRepository.createUserInfo(DummyData.userInfo)
+
+    await #expect(throws: RepositoryError.dataAlreadyExist, performing: {
+      try await userInfoRepository.createUserInfo(DummyData.userInfo)
+    })
+  }
+  
+  @Test("UserInfo Fetch Error Test - Data Not Found")
+  func fetchUserInfoDataNotFound() async throws {
+    await #expect(throws: RepositoryError.dataNotFound, performing: {
+      _ = try await userInfoRepository.fetchUserInfo()
+    })
+  }
+  
+  @Test("UserInfo Update Test")
+  func updateUserInfo() async throws {
+    
+    try await userInfoRepository.createUserInfo(DummyData.userInfo)
+    
+    let updatedUserInfo = UserInfo(
+      nickname: "업데이트 데이타 ㅋ",
+      categories: [Category(id: 999, name: "테스트")],
+      recentKeywords: [],
+      generateCount: 7,
+      lastStreakUpdatedAt: Date(),
+      streak: []
+    )
+
+    try await userInfoRepository.updateUserInfo(updatedUserInfo)
+
+    let fetchedUserInfo = try await userInfoRepository.fetchUserInfo()
+    #expect(fetchedUserInfo == updatedUserInfo)
+  }
+  
+  @Test("UserInfo Update Error Test - Data Not Found")
+  func updateUserInfoDataNotFound() async throws {
+    let nonExistentUser = DummyData.userInfo
+
+    await #expect(throws: RepositoryError.dataNotFound, performing: {
+      try await userInfoRepository.updateUserInfo(nonExistentUser)
+    })
+  }
+  
+  @Test("UserInfo Delete Test")
+  func deleteUserInfo() async throws {
+    
+    try await userInfoRepository.createUserInfo(DummyData.userInfo)
+    try await userInfoRepository.deleteUserInfo()
+    
+    await #expect(throws: RepositoryError.dataNotFound, performing: {
+      _ = try await userInfoRepository.fetchUserInfo()
+    })
+  }
+  
+  @Test("UserInfo Delete Error Test - Data Not Found")
+  func deleteUserInfoDataNotFound() async throws {
+    await #expect(throws: RepositoryError.dataNotFound, performing: {
+      try await userInfoRepository.deleteUserInfo()
+    })
+  }
+}
