6b77b9b29467bf0641bfff107998ac083da64478
shinseungjae <mok05247@naver.com>
2025-05-17 10:29:40 +0900

    [#45] Swift Testing 모듈 추가

--- B.READ/B.READ/Sources/Data/Impls/UserInfoRepositoryImpl.swift
+++ B.READ/B.READ/Sources/Data/Impls/UserInfoRepositoryImpl.swift
@@ -6,23 +6,23 @@
 //
 
 import Foundation
-
-final class UserInfoRepositoryImpl: UserInfoRepository {
-  func createUserInfo(_ userInfo: UserInfo) async throws {
-    print("Impl: ", #function)
-  }
-  
-  func fetchUserInfo() async throws -> UserInfo {
-    print("Impl: ", #function)
-  }
-  
-  func updateUserInfo(_ userInfo: UserInfo) async throws {
-    print("Impl: ", #function)
-  }
-  
-  func deleteUserInfo() async throws {
-    print("Impl: ", #function)
-  }
-  
-  
-}
+//import SwiftData
+//
+//@ModelActor
+//actor UserInfoRepositoryImpl: UserInfoRepository {
+//  func createUserInfo(_ userInfo: UserInfo) async throws {
+//    print("Impl: ", #function)
+//  }
+//  
+//  func fetchUserInfo() async throws -> UserInfo {
+//    print("Impl: ", #function)
+//  }
+//  
+//  func updateUserInfo(_ userInfo: UserInfo) async throws {
+//    print("Impl: ", #function)
+//  }
+//  
+//  func deleteUserInfo() async throws {
+//    print("Impl: ", #function)
+//  }
+//}
