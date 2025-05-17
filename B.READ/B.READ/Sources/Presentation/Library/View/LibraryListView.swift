01f2eda0816db8135a8153e34dec656333ea060b
심근웅 <geunwoong.sim@gmail.com>
2025-05-17 18:40:28 +0900

    [#38] 파일 폴더링 및 리스트, 그리드 파일 추가

---
+++ B.READ/B.READ/Sources/Presentation/Library/View/LibraryListView.swift
@@ -0,0 +1,20 @@
+//
+//  LibraryListView.swift
+//  B.READ
+//
+//  Created by 심근웅 on 5/17/25.
+//
+
+import SwiftUI
+
+struct LibraryListView: View {
+  var body: some View {
+    Text("Hello List View!")
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(.orange.opacity(0.3))
+  }
+}
+
+#Preview {
+  LibraryListView()
+}
