20f79bdce2c78bb3f8ca4f09f0e1089348b4e219
shinseungjae <mok05247@naver.com>
2025-05-18 10:52:21 +0900

    [#45] SwiftDataTestContainer 구현

---
+++ B.READ/B.READTests/Container/SwiftDataTestContainer.swift
@@ -0,0 +1,30 @@
+//
+//  SwiftDataTestContainer.swift
+//  B.READTests
+//
+//  Created by 신승재 on 5/18/25.
+//
+
+import Foundation
+import SwiftData
+
+final class SwiftDataTestContainer {
+  let modelContainer = {
+    let schema = Schema(
+      [UserInfoDTO.self]
+    )
+    let configuration = ModelConfiguration(
+      isStoredInMemoryOnly: true
+    )
+    
+    do {
+      let container = try ModelContainer(
+        for: schema,
+        configurations: configuration
+      )
+      return container
+    } catch {
+      fatalError("Model Container error")
+    }
+  }()
+}
