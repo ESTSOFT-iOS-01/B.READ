2856b33b7bc143f7bb9dfb94f8156faf56da962a
심근웅 <geunwoong.sim@gmail.com>
2025-05-18 16:45:06 +0900

    [#38] Book 더미 데이터 딕셔너리 형태로 변경

--- B.READ/B.READ/Sources/Presentation/Library/Dummy/BookDummy.swift
+++ B.READ/B.READ/Sources/Presentation/Library/Dummy/BookDummy.swift
@@ -25,8 +25,8 @@ struct Book {
   let totalPages: Int
 }
 
-let dummyBooks: [Book] = [
-  Book(
+let dummyBooks: [String: Book] = [
+  "9791194368137": Book(
     isbn: "9791194368137",
     name: "워런 버핏 웨이",
     author: "로버트 해그스트롬",
@@ -34,7 +34,7 @@ let dummyBooks: [Book] = [
     publishedAt: Calendar.current.date(from:DateComponents(year: 2025, month: 5, day: 21))!,
     totalPages: 500
   ),
-  Book(
+  "9791158510619": Book(
     isbn: "9791158510619",
     name: "타이탄의 도구들",
     author: "팀 페리스",
@@ -42,7 +42,7 @@ let dummyBooks: [Book] = [
     publishedAt: Calendar.current.date(from:DateComponents(year: 2022, month: 6, day: 22))!,
     totalPages: 367
   ),
-  Book(
+  "9788937460586": Book(
     isbn: "9788937460586",
     name: "싯다르타",
     author: "헤르만헤세",
@@ -50,7 +50,7 @@ let dummyBooks: [Book] = [
     publishedAt: Calendar.current.date(from: DateComponents(year: 2002, month: 1, day: 20))!,
     totalPages: 252
   ),
-  Book(
+  "9791162540640": Book(
     isbn: "9791162540640",
     name: "아주 작은 습관들",
     author: "제임스 클리어",
@@ -58,7 +58,7 @@ let dummyBooks: [Book] = [
     publishedAt: Calendar.current.date(from: DateComponents(year: 2019, month: 2, day: 26))!,
     totalPages: 360
   ),
-  Book(
+  "9791192372730": Book(
     isbn: "9791192372730",
     name: "위버멘쉬",
     author: "프리드리히 니체",
@@ -66,7 +66,15 @@ let dummyBooks: [Book] = [
     publishedAt: Calendar.current.date(from: DateComponents(year: 2025, month: 3, day: 14))!,
     totalPages: 260
   ),
-  Book(
+  "9788932473901": Book(
+    isbn: "9788932473901",
+    name: "이기적 유전자",
+    author: "리처드 도킨스",
+    publisher: "을유문화사",
+    publishedAt: Calendar.current.date(from: DateComponents(year: 2018, month: 10, day: 20))!,
+    totalPages: 632
+  ),
+  "9791194368175": Book(
     isbn: "9791194368175",
     name: "듀얼 브레인",
     author: "이선 몰릭",
@@ -74,7 +82,4 @@ let dummyBooks: [Book] = [
     publishedAt: Calendar.current.date(from: DateComponents(year: 2025, month: 3, day: 19))!,
     totalPages: 308
   )
-  
-  
-  
 ]
