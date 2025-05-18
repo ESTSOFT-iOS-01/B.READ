d3e1715a81ccfd9568ae539735518b3ce0a79c06
심근웅 <geunwoong.sim@gmail.com>
2025-05-19 00:40:08 +0900

    [#52] 독서기록조회 - 네비게이션바 구현 및 표지 들어갈 영역 표시

---
+++ B.READ/B.READ/Sources/Presentation/Library/RecordDetailView.swift
@@ -0,0 +1,102 @@
+//
+//  RecordDetailView.swift
+//  B.READ
+//
+//  Created by 심근웅 on 5/18/25.
+//
+
+import SwiftUI
+
+// MARK: - (S)RecordDetailView
+struct RecordDetailView: View {
+  
+  @Environment(\.dismiss) var dismiss
+  
+  @Binding var isFavorite: Bool
+  @State var showDeleteAlert: Bool = false
+  
+  init(isFavorite: Binding<Bool>) {
+    self._isFavorite = isFavorite
+  }
+  
+  var body: some View {
+    ScrollView(.vertical) {
+      DetailView()
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+//        .background(.orange3.opacity(0.3))
+        .padding(.top, 24)
+        .padding(.horizontal, 24)
+    } // : VStack
+    .navigationBarTitleDisplayMode(.inline)
+    .navigationBarBackButtonHidden(true) // 기본 뒤로가기 버튼 숨김
+    .toolbar {
+      // 뒤로가기 버튼
+      ToolbarItem(placement: .topBarLeading) {
+        BackButton()
+      }
+      
+      ToolbarItem(placement: .topBarTrailing) {
+        HStack(spacing: 0) {
+          Button {
+            print("즐겨찾기 버튼 클릭")
+            self.isFavorite.toggle()
+          } label: {
+            Image(systemName: isFavorite ? "bookmark.fill" : "bookmark")
+          }
+          Button {
+            print("삭제 버큰 클릭")
+            showDeleteAlert = true
+          } label: {
+            Text("삭제")
+              .brStyleFont(.pretendard(.regular, size: 16), lineHeight: 1.4)
+          }
+        } // HStack
+        .foregroundColor(.green6)
+      } // : ToolbarItem
+    } // : .toolBar
+    .alert("기록삭제", isPresented: $showDeleteAlert) {
+      // TODO: - 삭제 alert뷰 구성
+      Button("삭제", role: .destructive) {
+        print("삭제 진행")
+        // TODO: - (2)삭제 로직 구현
+        dismiss()
+      }
+      Button("취소", role: .cancel) { }
+    }
+  }
+}
+
+// MARK: - (S)BackButton
+struct BackButton: View {
+  @Environment(\.dismiss) var dismiss
+  
+  var body: some View {
+    Button(action: {
+      dismiss()
+    }) {
+      Image(systemName: "chevron.left")
+        .foregroundColor(.green6) // ← 원하는 색상 지정
+        .imageScale(.large)
+    }
+  }
+}
+
+// MARK: - (S)DetailView
+struct DetailView: View {
+  var body: some View {
+    VStack {
+      Rectangle()
+        .fill(.blue.opacity(0.3))
+        .frame(width: 176, height: 284)
+        .cornerRadius(6)
+      
+      // TODO: - 여기서 부터 책제목 들어감
+    } // : VStack
+  }
+}
+
+#Preview {
+  @Previewable @State var isFavorite = true
+//  RecordDetailView(isFavorite: $isFavorite)
+  LibraryView(isFavorite: isFavorite)
+}
