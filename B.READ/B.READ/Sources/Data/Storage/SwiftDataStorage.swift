cbcffad9ecf942ca459256f7a7b0b1f6a93e3ede
shinseungjae <mok05247@naver.com>
2025-05-18 11:05:24 +0900

    [#45] SwiftDataStorage 구현

---
+++ B.READ/B.READ/Sources/Data/Storage/SwiftDataStorage.swift
@@ -0,0 +1,30 @@
+//
+//  SwiftDataStorage.swift
+//  B.READ
+//
+//  Created by 신승재 on 5/18/25.
+//
+
+import Foundation
+import SwiftData
+
+final class SwiftDataStorage {
+  let modelContainer = {
+    let schema = Schema(
+      [UserInfoDTO.self]
+    )
+    let configuration = ModelConfiguration(
+      isStoredInMemoryOnly: false
+    )
+    
+    do {
+      let container = try ModelContainer(
+        for: schema,
+        configurations: configuration
+      )
+      return container
+    } catch {
+      fatalError("Model Container error")
+    }
+  }()
+}
