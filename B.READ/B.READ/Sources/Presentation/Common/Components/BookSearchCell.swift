f3231d06ec2850b62f34dbebfb6e60fec0a7fcd0
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-05-19 10:47:27 +0900

    [#41] 책 검색 셀 데이터 변경

--- B.READ/B.READ/Sources/Presentation/Common/Components/BookSearchCell.swift
+++ B.READ/B.READ/Sources/Presentation/Common/Components/BookSearchCell.swift
@@ -8,16 +8,11 @@
 import SwiftUI
 
 struct BookSearchCell: View {
-  
-  let coverImage: Image
-  let title: String
-  let author: String
-  let publisher: String
-  let publishedDate: Date
+  let data: BookVO
   
   var body: some View {
     HStack(alignment: .top, spacing: 24) {
-      coverImage
+      data.coverImage
         .resizable()
         .aspectRatio(contentMode: .fill)
         .frame(width: 58 ,height: 88)
@@ -25,18 +20,18 @@ struct BookSearchCell: View {
         .shadow(color: .black.opacity(0.25), radius: 5, x: 0, y: 2)
       
       VStack(alignment: .leading, spacing: 8) {
-        Text(title)
+        Text(data.title)
           .foregroundStyle(.gray9)
           .brStyleFont(.pretendard(.semiBold, size: 18), lineHeight: 1.0)
           .lineLimit(1)
           .truncationMode(.tail)
         
-        Text(author)
+        Text(data.author)
           .foregroundStyle(.brown8)
           .brStyleFont(.pretendard(.medium, size: 14), lineHeight: 1.15)
           .lineLimit(1)
           .truncationMode(.tail)
-        Text("\(publisher) | \(publishedDate.string(format: .dotSeparated))")
+        Text("\(data.publisher) | \(data.publishedDate.string(format: .dotSeparated))")
           .foregroundStyle(.brown5)
           .brStyleFont(.pretendard(.regular, size: 12), lineHeight: 1.15, letterSpacing: -0.025)
           .lineLimit(1)
@@ -52,26 +47,16 @@ struct BookSearchCell: View {
   }
 }
 
-#Preview {
-  BookSearchCell(
-    coverImage: Image(.exampleBook),
-    title: "데미안",
-    author: "헤르만 헤세",
-    publisher: "민음사",
-    publishedDate: Date()
-  )
-  BookSearchCell(
-    coverImage: Image(.exampleBook),
-    title: "데미안",
-    author: "헤르만 헤세",
-    publisher: "민음사",
-    publishedDate: Date()
-  )
-  BookSearchCell(
-    coverImage: Image(.exampleBook),
-    title: "데미안",
-    author: "헤르만 헤세",
-    publisher: "민음사",
-    publishedDate: Date()
-  )
-}
+//#Preview {
+//  let dummy = BookVO(
+//    isbn: "1234567890",
+//    coverImage: Image(.exampleBook),
+//    title: "데미안",
+//    author: "헤르만 헤세",
+//    publisher: "민음사",
+//    publishedDate: Date()
+//  )
+//  BookSearchCell(data: dummy)
+//  BookSearchCell(data: dummy)
+//  BookSearchCell(data: dummy)
+//}
