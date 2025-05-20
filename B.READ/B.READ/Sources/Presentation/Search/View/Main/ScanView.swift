545d22b0515283ec6160ed0681b47ec7d274eb04
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-05-20 11:37:36 +0900

    [#56] 스캔 기본 화면 UI 완성

---
+++ B.READ/B.READ/Sources/Presentation/Search/View/Main/ScanView.swift
@@ -0,0 +1,53 @@
+//
+//  ScanView.swift
+//  B.READ
+//
+//  Created by 김도연 on 5/20/25.
+//
+
+import SwiftUI
+
+struct ScanView: View {
+    var body: some View {
+      NavigationStack {
+        VStack {
+          Rectangle()
+            .fill(.gray3)
+            .frame(width: .infinity, height: 400, alignment: .top)
+            .padding(.top, 16)
+          
+          Text("빵식이가 빠르게 책을 찾을 수 있도록\n책의 ISBN 바코드를 정확히 스캔해주세요!")
+            .brStyleFont(.pretendard(.semiBold, size: 16), lineHeight: 1.2, letterSpacing: 0.02)
+            .multilineTextAlignment(.center)
+            .foregroundStyle(.brown5)
+            .frame(alignment: .center)
+            .padding(.top, 32)
+          
+          VStack(spacing: 8) {
+            Text("ISBN 예시")
+              .brStyleFont(.pretendard(.regular, size: 14), lineHeight: 1.2, letterSpacing: -0.01)
+              .foregroundStyle(.brown8)
+              .frame(alignment: .center)
+              .padding(.top, 4)
+              
+            Image(SearchConstants.Image.isbnEx)
+              .resizable()
+              .aspectRatio(contentMode: .fit)
+              .frame(width: 200, alignment: .center)
+              .padding(.horizontal, 8)
+          }
+          .border(.brown5)
+          .padding(.top, 20)
+        }
+        .frame(maxHeight: .infinity, alignment: .top)
+        .navigationTitle("ISBN 바코드 스캔")
+        .navigationBarBackButtonHidden(false)
+        .navigationBarTitleDisplayMode(.inline)
+      }
+      .background(.backgroundDefault, ignoresSafeAreaEdges: .all)
+    }
+}
+
+#Preview {
+    ScanView()
+}
