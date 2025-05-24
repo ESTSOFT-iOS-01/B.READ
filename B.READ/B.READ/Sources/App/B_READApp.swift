2ae5c1af8fad2995f0cc6c933a7ae55b2afa9240
shinseungjae <mok05247@naver.com>
2025-05-24 12:19:18 +0900

    [#71] RootSwitcher 추가

--- B.READ/B.READ/Sources/App/B_READApp.swift
+++ B.READ/B.READ/Sources/App/B_READApp.swift
@@ -9,20 +9,10 @@ import SwiftUI
 
 @main
 struct B_READApp: App {
-  @State private var coordinator = Coordinator<OnboardingRoute>()
-  @AppStorage("didInitialSetup") private var didInitialSetup: Bool = true
   
   var body: some Scene {
     WindowGroup {
-      Group {
-        if didInitialSetup {
-          OnBoardingView()
-            .environmentObject(coordinator)
-        } else {
-          MainTabView()
-            .transition(.move(edge: .trailing))
-        }
-      }.animation(.linear(duration: 0.3), value: didInitialSetup)
+      RootViewSwitcher()
     }
   }
 }
