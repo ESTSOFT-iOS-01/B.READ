40f6f934226147af0d2f790cb39b8d52d2b2eceb
shinseungjae <mok05247@naver.com>
2025-05-25 20:47:29 +0900

    [#82] Memo View 추가

---
+++ B.READ/B.READ/Sources/Presentation/Memo/MemoView.swift
@@ -0,0 +1,20 @@
+//
+//  MemoView.swift
+//  B.READ
+//
+//  Created by 신승재 on 5/25/25.
+//
+
+import SwiftUI
+
+struct MemoView: View {
+  var body: some View {
+    VStack {
+      Text("빵식이의 가이드")
+    }
+  }
+}
+
+#Preview {
+  MemoView()
+}
