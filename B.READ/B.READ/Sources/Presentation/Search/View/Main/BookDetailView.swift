8ab905c1b3ac1085bfc2ac62e06f0ca6f54ce2cc
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-05-27 17:09:57 +0900

    [#89] 도서 조회 전체 화면 UI 조합

--- B.READ/B.READ/Sources/Presentation/Search/View/Main/BookDetailView.swift
+++ B.READ/B.READ/Sources/Presentation/Search/View/Main/BookDetailView.swift
@@ -13,24 +13,45 @@ struct BookDetailView: View {
   
   var body: some View {
     VStack {
-      Text(viewModel.text)
-        .padding()
-      
-      Text(viewModel.isbn)
-        .padding()
-      
-      Button {
-        coordinator.pop()
-      } label: {
-        Text("pop page")
-          .padding()
-          .border(.brown7)
+      ScrollView {
+        VStack(alignment: .center, spacing: 16) {
+          LargeImageView(imageURL: ImageURLConverter.highQualityURL(from: viewModel.bookVO.coverURL))
+            .padding(.bottom, 24)
+          
+          BookTitleView(
+            title: viewModel.bookVO.title,
+            author: viewModel.bookVO.author,
+            publisher: viewModel.bookVO.publisher,
+            page: viewModel.bookVO.pageCount,
+            date: viewModel.bookVO.publishedDate.toDotDateFormat()
+          )
+          
+          BookRateView(count: viewModel.bookVO.ratingCount, rate: viewModel.bookVO.ratingScore)
+          
+          BookInfoView(title: "ISBN", content: viewModel.bookVO.isbn)
+          BookInfoView(title: "상세 정보", content: viewModel.bookVO.description)
+          
+          LinkView(action: { print("\(viewModel.bookVO.link)로 이동") })
+            .padding(.bottom, 40)
+        }
       }
-
+      .scrollIndicators(.hidden)
+      
+      BottomButton(
+        buttonTitle: "내 책빵에 저장하기",
+        action: {
+          print("저장 버튼 눌림")
+        }
+      )
+      .padding(.horizontal, 30)
+      .padding(.top, 8)
+      .padding(.bottom, 16)
+      
     }
+    .background(.backgroundDefault)
   }
 }
 
-//#Preview {
-//  BookDetailView()
-//}
+#Preview {
+  BookDetailView(viewModel: BookViewModel(isbn: "9791187011590"))
+}
