8d6723376ac91b769ac110588e2041a40a6b3a93
shinseungjae <mok05247@naver.com>
2025-05-28 15:14:48 +0900

    [#82] page 필터 기능 추가

--- B.READ/B.READ/Sources/Presentation/Memo/MemoView.swift
+++ B.READ/B.READ/Sources/Presentation/Memo/MemoView.swift
@@ -9,15 +9,52 @@ import SwiftUI
 
 struct MemoView: View {
   
+  @State private var startPage: String = ""
+  @State private var endPage: String = ""
+  @State private var memoText: String = ""
+  
   let targetDate: Date
-  let guideText = """
+  let totalPage: Int
+  
+  private let guideText = """
   여기를 터치해서
   빵식이가 제안하는
   질문을 볼 수 있어요
   """
   
   var body: some View {
-    VStack(alignment: .leading, spacing: 0) {
+    VStack(alignment: .leading, spacing: 24) {
+      
+      guideSection()
+      
+      pageSection()
+      
+      memoSection()
+      
+    }
+    .navigationTitle(targetDate.string(format: .dotSeparatedFull))
+    .frame(maxHeight: .infinity, alignment: .top)
+    .padding(.horizontal, 24)
+    .onChange(of: startPage) {
+      startPage = formatDigits($1)
+    }
+    .onChange(of: endPage) {
+      endPage = formatDigits($1)
+    }
+  }
+  
+  private func formatDigits(_ input: String) -> String {
+    let filtered = input.filter { $0.isNumber }
+    if filtered.hasPrefix("0"), filtered.count >= 1 {
+      return String(filtered.drop(while: { $0 == "0" }))
+    }
+    return filtered
+  }
+  
+  // MARK: (F)guideSection
+  @ViewBuilder
+  private func guideSection() -> some View {
+    VStack(alignment: .leading, spacing: 8) {
       Text("빵식이의 가이드")
         .foregroundStyle(.black)
         .brStyleFont(.pretendard(.semiBold, size: 18), lineHeight: 1.2)
@@ -32,30 +69,48 @@ struct MemoView: View {
           .foregroundStyle(.gray5)
           .brStyleFont(.pretendard(.regular, size: 14), lineHeight: 1.4, letterSpacing: -0.025)
           .frame(maxWidth: .infinity, alignment: .leading)
-          
       }
       .padding(.vertical, 25)
       .padding(.horizontal, 37)
       .frame(maxWidth: .infinity)
       .background(.green1)
       .clipShape(RoundedRectangle(cornerRadius: 8))
-      .padding(.top, 8)
-      
+    }
+  }
+  
+  // MARK: (F)pageSection
+  @ViewBuilder
+  private func pageSection() -> some View {
+    VStack(alignment: .leading, spacing: 16) {
       Text("페이지")
         .foregroundStyle(.black)
         .brStyleFont(.pretendard(.semiBold, size: 18), lineHeight: 1.2)
-        .padding(.top, 24)
       
+      HStack {
+        RoundedTextField(type: .pages, placeholder: "0", text: $startPage, isValid: true)
+        
+        Text("~")
+        
+        RoundedTextField(type: .pages, placeholder: "0", text: $endPage, isValid: true)
+        
+        Text("쪽")
+      }.padding(.trailing, 52)
+    }
+  }
+  
+  // MARK: (F)memoSection
+  @ViewBuilder
+  private func memoSection() -> some View {
+    VStack(alignment: .leading, spacing: 16) {
       Text("메모 내용을 작성해주세요")
         .foregroundStyle(.black)
         .brStyleFont(.pretendard(.semiBold, size: 18), lineHeight: 1.2)
-        .padding(.top, 24)
+      
+     // TODO: 텍스트 에디터 들어오면 넣기
     }
-    .navigationTitle(targetDate.string(format: .dotSeparatedFull))
-    .padding(.horizontal, 24)
   }
 }
 
 #Preview {
-  MemoView(targetDate: .now)
+  MemoView(targetDate: .now, totalPage: 200)
 }
