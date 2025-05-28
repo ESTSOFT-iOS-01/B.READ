d796bf10c304b3332d0e2169a2b18e8b8eb3e5fb
심근웅 <geunwoong.sim@gmail.com>
2025-05-28 11:47:22 +0900

    [#96] 헤더 여백 수정

--- B.READ/B.READ/Sources/Presentation/Record/View/QuoteListView.swift
+++ B.READ/B.READ/Sources/Presentation/Record/View/QuoteListView.swift
@@ -23,14 +23,12 @@ struct QuoteListView: View {
               }
               .padding(.leading, 8)
             }
-            
-            Color.clear // 책의 마지막 문장과 다음책의 헤더 사이의 공간
-              .frame(height: 16)
           } header: {
             Text(group.bookTitle)
               .brStyleFont(.pretendard(.semiBold, size: 18), lineHeight: 1.0)
               .frame(maxWidth: .infinity, alignment: .leading)
               .background(.backgroundDefault)
+              .padding(.top, 24)
           }// : Section
         }
         
