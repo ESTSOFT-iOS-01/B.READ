a0042d3b65064546d9f8093a86e649ccad067228
shinseungjae <mok05247@naver.com>
2025-05-28 16:47:00 +0900

    [#82] Regular로 수정

--- B.READ/B.READ/Sources/Util/Extensions/UINavigationBar+.swift
+++ B.READ/B.READ/Sources/Util/Extensions/UINavigationBar+.swift
@@ -18,7 +18,7 @@ extension UINavigationBar {
       .foregroundColor: UIColor.black
     ]
     
-    let config = UIImage.SymbolConfiguration(pointSize: 20, weight: .regular)
+    let config = UIImage.SymbolConfiguration(pointSize: 20, weight: .light)
     let chevronImage = UIImage(systemName: "chevron.backward", withConfiguration: config)?
       .withTintColor(.green6, renderingMode: .alwaysOriginal)
       .withAlignmentRectInsets(UIEdgeInsets(top: 0, left: -8, bottom: 0, right: 0))
