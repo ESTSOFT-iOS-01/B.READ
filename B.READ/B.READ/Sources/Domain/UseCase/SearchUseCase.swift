71ed8f1ab58dbb8652684ddd2bd2acc1c35b659d
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-05-30 14:32:02 +0900

    [#106] SearchUseCaseImpl 구현

--- B.READ/B.READ/Sources/Domain/UseCase/SearchUseCase.swift
+++ B.READ/B.READ/Sources/Domain/UseCase/SearchUseCase.swift
@@ -12,8 +12,8 @@ protocol SearchUseCase {
   func searchBookDetail(isbn: String) async throws -> BookDetail
 
   /// 쿼리로 외부 API에서 책 목록을 검색한다.
-  func searchBooksFromService(query: String) async throws -> SearchPagnation
+  func searchBooksFromService(query: String, page: Int) async throws -> SearchPagnation
 
   /// 쿼리로 내부 저장소에서 등록된 책 목록을 검색한다.
-  func searchBooksFromRepository(query: String) async throws -> [Record]
+  func searchBooksFromRepository(query: String) async throws -> [(Record, Book)]
 }
