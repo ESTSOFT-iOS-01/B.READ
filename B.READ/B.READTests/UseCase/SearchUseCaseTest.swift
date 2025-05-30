f7e1afc9cd576452592081abf7415bbfd434013a
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-05-30 15:46:57 +0900

    [#106] SearchUseCaseImpl Test 작성

--- B.READ/B.READTests/UseCase/SearchUseCaseTest.swift
+++ B.READ/B.READTests/UseCase/SearchUseCaseTest.swift
@@ -29,13 +29,55 @@ struct SearchUseCaseTest {
   }
   
   @Test("ISBN으로 책 상세 조회 성공")
-  func isbn으로_책_상세_조회() async throws {
+  func searchBookByISBN() async throws {
+    // given
+    let title = "데미안 (오리지널 초판본 표지디자인) - 최신 원전 완역본"
+    
     // when
     let detail = try await searchUseCase.searchBookDetail(isbn: "9791187011590")
     
     // then
-    #expect(detail.title == "데미안 (오리지널 초판본 표지디자인) - 최신 원전 완역본")
+    #expect(detail.title == title)
   }
   
+  @Test("레포에서 키워드로 기록 조회 성공")
+  func searchRecordByKeywordSuccess() async throws {
+    // given
+    for book in DummyData.books {
+      try await bookRepository.createBook(book)
+    }
+    
+    for record in DummyData.records {
+      try await recordRepository.createRecord(record)
+    }
+    
+    // when
+    let results = try await searchUseCase.searchBooksFromRepository(query: "타이탄")
+    
+    // then
+    #expect(results.count == 1)
+    #expect(results.first?.1.name == "타이탄의 도구들")
+    #expect(results.first?.0.isbn == "9791158510619")
+  }
+  
+  
+  @Test("쿼리에 해당하는 책이 없을 때 검색 결과는 빈 배열이어야 한다")
+  func searchRecordByKeywordEmptyResults() async throws {
+    // given
+    for book in DummyData.books {
+      try await bookRepository.createBook(book)
+    }
+    
+    for record in DummyData.records {
+      try await recordRepository.createRecord(record)
+    }
+
+    // when
+    let results = try await searchUseCase.searchBooksFromRepository(query: "존재하지않는책")
+
+    // then
+    #expect(results.isEmpty)
+  }
+
 }
 
