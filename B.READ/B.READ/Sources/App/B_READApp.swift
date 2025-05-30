89af7559dcbd227957f88037a33c14f265d2cbff
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-05-30 11:32:24 +0900

    [#103] 여러가지 시도

--- B.READ/B.READ/Sources/App/B_READApp.swift
+++ B.READ/B.READ/Sources/App/B_READApp.swift
@@ -10,13 +10,13 @@ import SwiftUI
 @main
 struct B_READApp: App {
   
-  init() {
-    // 커스텀 ValueTransformer 등록
-    ValueTransformer.setValueTransformer(
-      StringArrayTransformer(),
-      forName: NSValueTransformerName("StringArrayTransformer")
-    )
-  }
+//  init() {
+//    // 커스텀 ValueTransformer 등록
+//    ValueTransformer.setValueTransformer(
+//      StringArrayTransformer(),
+//      forName: NSValueTransformerName("StringArrayTransformer")
+//    )
+//  }
   
   var body: some Scene {
     WindowGroup {
--- B.READ/B.READ/Sources/App/StringArrayTransformer.swift
+++ B.READ/B.READ/Sources/App/StringArrayTransformer.swift
@@ -7,23 +7,25 @@
 
 import Foundation
 
-@objc(StringArrayTransformer)
-final class StringArrayTransformer: ValueTransformer {
-  override class func transformedValueClass() -> AnyClass {
-    return NSData.self
-  }
-  
-  override class func allowsReverseTransformation() -> Bool {
-    return true
-  }
-  
-  override func transformedValue(_ value: Any?) -> Any? {
-    guard let array = value as? [String] else { return nil }
-    return try? JSONEncoder().encode(array)
-  }
-  
-  override func reverseTransformedValue(_ value: Any?) -> Any? {
-    guard let data = value as? Data else { return nil }
-    return try? JSONDecoder().decode([String].self, from: data)
-  }
-}
+//@objc(StringArrayTransformer)
+//final class StringArrayTransformer: ValueTransformer {
+//  override class func transformedValueClass() -> AnyClass {
+//    return NSData.self
+//  }
+//  
+//  override class func allowsReverseTransformation() -> Bool {
+//    return true
+//  }
+//  
+//  override func transformedValue(_ value: Any?) -> Any? {
+//      guard let array = value as? [String] else {
+//          return try? JSONEncoder().encode("")
+//      }
+//      return try? JSONEncoder().encode(array)
+//  }
+//  
+//  override func reverseTransformedValue(_ value: Any?) -> Any? {
+//    guard let data = value as? Data else { return nil }
+//    return try? JSONDecoder().decode([String].self, from: data)
+//  }
+//}
