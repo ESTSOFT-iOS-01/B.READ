cca3693972510fd4835f3fe026947ea5496a3904
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-06-02 14:56:07 +0900

    [#126] RecommandUseCaseTest 복구 완료

--- B.READ/UsecaseTest/RecommandUseCaseTest.swift
+++ B.READ/UsecaseTest/RecommandUseCaseTest.swift
@@ -11,39 +11,39 @@ import Testing
 @testable import B_READ
 
 struct RecommandUseCaseTest {
-//  private let recommandUseCase: RecommandUseCase
-//  private let bookService: BookService
-//  
-//  init() {
-//    let storage = SwiftDataTestStorage()
-//    bookService = AladinService()
-//    
-//    recommandUseCase = RecommandUseCaseImpl(bookService: bookService)
-//  }
-//  
-//  @Test("베스트셀러 조회 성공")
-//  func fetchBestSellerSuccess() async throws {
-//    // given
-//    let category = 0 // 전체 카테고리
-//
-//    // when
-//    let books = try await recommandUseCase.requestBestSeller(in: category)
-//
-//    // then
-//    #expect(!books.isEmpty)
-//  }
-//  
-//  @Test("잘못된 카테고리로 조회 시 실패")
-//  func fetchBestSellerInvalidCategory() async throws {
-//    // given
-//    let invalidCategory = 21
-//
-//    do {
-//      _ = try await recommandUseCase.requestBestSeller(in: invalidCategory)
-//      #expect(false) // 여기로 들어가면 실패..
-//    } catch {
-//      #expect(error is AladinError)
-//    }
-//  }
-//  
+  private let recommandUseCase: RecommandUseCase
+  private let bookService: BookService
+  
+  init() {
+    let storage = SwiftDataTestStorage()
+    bookService = AladinService()
+    
+    recommandUseCase = RecommandUseCaseImpl(bookService: bookService)
+  }
+  
+  @Test("베스트셀러 조회 성공")
+  func fetchBestSellerSuccess() async throws {
+    // given
+    let category = 0 // 전체 카테고리
+
+    // when
+    let books = try await recommandUseCase.requestBestSeller(in: category)
+
+    // then
+    #expect(!books.isEmpty)
+  }
+  
+  @Test("잘못된 카테고리로 조회 시 실패")
+  func fetchBestSellerInvalidCategory() async throws {
+    // given
+    let invalidCategory = 21
+
+    do {
+      _ = try await recommandUseCase.requestBestSeller(in: invalidCategory)
+      #expect(false) // 여기로 들어가면 실패..
+    } catch {
+      #expect(error is AladinError)
+    }
+  }
+  
 }
