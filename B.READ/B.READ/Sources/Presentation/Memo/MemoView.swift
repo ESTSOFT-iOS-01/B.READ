424f94bb48ae07ace818d655ddacc528955e29d0
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-06-04 09:52:14 +0900

    Squashed commit of the following:
    commit f82b6a0dd24ea4938cb0eccea707d72f8f881045
    Merge: 3c7bd0b 52483b4
    Author: 신승재 <68256612+Monfi98@users.noreply.github.com>
    Date:   Tue Jun 3 01:22:06 2025 +0900
    
        Merge pull request #128 from ESTSOFT-iOS-01/feature/#118
    
        [feature] MemoView 기능 구현 완료
    
    commit 52483b47669349d9369598b403141fa56646bb07
    Author: shinseungjae <mok05247@naver.com>
    Date:   Tue Jun 3 01:20:36 2025 +0900
    
        [#118] Trash SFSymbol 추가
    
    commit 32f78b78452bd45c52e315e8e66385baa7f5ea1e
    Author: shinseungjae <mok05247@naver.com>
    Date:   Tue Jun 3 01:17:51 2025 +0900
    
        [#118] generate 연결
    
    commit da6db5bf9f95d4b95a7ebcb5be6768d2444cdbd6
    Author: shinseungjae <mok05247@naver.com>
    Date:   Tue Jun 3 01:13:30 2025 +0900
    
        [#118] 빵식이 가이드 스크롤뷰로 변경
    
    commit 97531c865bce73e33fc711c40774d592a717b022
    Author: shinseungjae <mok05247@naver.com>
    Date:   Tue Jun 3 01:06:49 2025 +0900
    
        [#118] if let 으로 변경
    
    commit 22e3f447ee74a52c2216be3f28967f4f808d14aa
    Author: shinseungjae <mok05247@naver.com>
    Date:   Tue Jun 3 00:59:20 2025 +0900
    
        [#118] onChange 위치 변경
    
    commit c6065f537d808f09aad7c968e8cdd64dbda93270
    Author: shinseungjae <mok05247@naver.com>
    Date:   Tue Jun 3 00:57:12 2025 +0900
    
        [#118] focusField 삭제
    
    commit b1304e8b923b48b183030c27d2f2b7652721d6f6
    Author: shinseungjae <mok05247@naver.com>
    Date:   Tue Jun 3 00:51:48 2025 +0900
    
        [#118] 바뀐 UseCase 적용
    
    commit 7a8416a8374516347b5361e817b97e0c9c5bdb20
    Merge: 5fee7f6 3c7bd0b
    Author: shinseungjae <mok05247@naver.com>
    Date:   Tue Jun 3 00:41:57 2025 +0900
    
        Merge branch 'develop' of https://github.com/ESTSOFT-iOS-01/B.READ into feature/#118
    
    commit 5fee7f6e1ee62a5d3702aae5418b0d5cb72c6e0a
    Merge: ea46f14 3849774
    Author: 신승재 <68256612+Monfi98@users.noreply.github.com>
    Date:   Mon Jun 2 15:15:25 2025 +0900
    
        Merge branch 'develop' into feature/#118
    
    commit ea46f141a472de28de9363e1143ad277279d9bcc
    Author: shinseungjae <mok05247@naver.com>
    Date:   Mon Jun 2 15:06:29 2025 +0900
    
        [#118] Bullet Point 변경
    
    commit 873979b148977879c2ca9d1f9f107be7b547a33d
    Author: shinseungjae <mok05247@naver.com>
    Date:   Mon Jun 2 14:57:28 2025 +0900
    
        [#118] Guide status 추가 및 애니메이션 적용
    
    commit 682beb04fe1e2c70959fff646aed6de8611bfac6
    Author: shinseungjae <mok05247@naver.com>
    Date:   Mon Jun 2 13:10:27 2025 +0900
    
        [#118] 페이지 유효성검사 추가
    
    commit 4224e42e08dedbf312a8b3ca40b744f9a9437cda
    Author: shinseungjae <mok05247@naver.com>
    Date:   Mon Jun 2 10:34:40 2025 +0900
    
        [#118] alert 추가
    
    commit 78b18c1732c9bce2c74e63e3eebf41f7b4abc2c9
    Author: shinseungjae <mok05247@naver.com>
    Date:   Sun Jun 1 22:21:55 2025 +0900
    
        [#118] 화면 연결
    
    commit 6b03b3bb8990841235dd07d48bb54dcb19631c71
    Author: shinseungjae <mok05247@naver.com>
    Date:   Sun Jun 1 04:00:39 2025 +0900
    
        [#118] MemoViewModel에서 id 받을 수 있게 수정
    
    commit d21f41b5dd82f6f7d22fcd45c5c459bf64568a5b
    Author: shinseungjae <mok05247@naver.com>
    Date:   Sun Jun 1 03:40:10 2025 +0900
    
        [#118] MemoRepository Register
    
    commit cbe27bcbe3b257aaec33abe1fbdbde4c49b432a1
    Author: shinseungjae <mok05247@naver.com>
    Date:   Sun Jun 1 03:34:45 2025 +0900
    
        [#118] MemoViewModel 생성

--- B.READ/B.READ/Sources/Presentation/Memo/MemoView.swift
+++ B.READ/B.READ/Sources/Presentation/Memo/MemoView.swift
@@ -10,101 +10,81 @@ import SwiftUI
 struct MemoView: View {
   
   @EnvironmentObject var coordinator: Coordinator<MainRoute, SheetRoute>
-  @State private var startPage: String = ""
-  @State private var endPage: String = ""
-  @State private var memoText: String = "memo"
+  @StateObject private var viewModel: MemoViewModel
+  @State private var textEditorFocused: Bool = false
+  @State private var showGuideAlert = false
+  @State private var showErrorAlert = false
+  
+  private let totalPage: Int
+  
+  init(viewModel: MemoViewModel, totalPage: Int) {
+    self._viewModel = .init(wrappedValue: viewModel)
+    self.totalPage = totalPage
+  }
   
   private var isButtonEnabled: Bool {
-    !startPage.isEmpty && !endPage.isEmpty && !memoText.isEmpty
+    !viewModel.startPage.isEmpty && !viewModel.endPage.isEmpty && !viewModel.content.isEmpty
   }
-  
-  let targetDate: Date
-  let totalPage: Int
-  
-  private let guideText = """
-  여기를 터치해서
-  빵식이가 제안하는
-  질문을 볼 수 있어요
-  """
   
   var body: some View {
-    VStack(alignment: .leading, spacing: 24) {
-      
-      guideSection()
-      
-      pageSection()
-      
-      memoSection()
-      
-    }
-    .navigationTitle(targetDate.string(format: .dotSeparatedFull))
-    .frame(maxHeight: .infinity, alignment: .top)
-    .padding(.horizontal, 24)
-    .background(.backgroundDefault)
-    .onChange(of: startPage) {
-      startPage = formatDigits($1)
-    }
-    .onChange(of: endPage) {
-      endPage = formatDigits($1)
-    }
-    .toolbar {
-      ToolbarItem(placement: .topBarTrailing) {
-        Button {
-          coordinator.pop()
-        } label: {
-          Text("저장")
-            .brStyleFont(.pretendard(.regular, size: 16), lineHeight: 1.0)
-            .foregroundStyle(.green6)
-            .opacity(isButtonEnabled ? 1 : 0)
-            .disabled(!isButtonEnabled)
-            .animation(.easeInOut(duration: 0.2), value: isButtonEnabled)
+    ScrollView {
+      VStack(alignment: .leading, spacing: 24) {
+        
+        GuideSectionView(viewModel: viewModel, showGuideAlert: $showGuideAlert)
+        
+        pageSection()
+        
+        memoSection()
+        
+      }
+      .id("bottom")
+      .navigationTitle(viewModel.createAt.string(format: .dotSeparatedFull))
+      .frame(maxHeight: .infinity, alignment: .top)
+      .padding(.horizontal, 24)
+      .animation(.easeOut(duration: 0.25), value: viewModel.guideStatus)
+      .onChange(of: viewModel.isSaveComplete) {
+        coordinator.pop()
+      }
+      .toolbar {
+        ToolbarItem(placement: .topBarTrailing) {
+          Button {
+            if let start = Int(viewModel.startPage),
+               let end = Int(viewModel.endPage), start <= end, end <= totalPage {
+              viewModel.send(.saveMemo)
+            } else {
+              showErrorAlert = true
+            }
+          } label: {
+            Text("저장")
+              .brStyleFont(.pretendard(.regular, size: 16), lineHeight: 1.0)
+              .foregroundStyle(.green6)
+              .opacity(isButtonEnabled ? 1 : 0)
+              .disabled(!isButtonEnabled)
+              .animation(.easeInOut(duration: 0.2), value: isButtonEnabled)
+          }
         }
       }
+      .alert("가이드를 삭제하시겠습니까?", isPresented: $showGuideAlert) {
+        Button("취소", role: .cancel) { }
+        Button("삭제", role: .destructive) { viewModel.send(.deleteGuides) }
+      } message: {
+        Text("빵식이의 가이드를 삭제하시겠습니까?\n(다시 생성되지 않습니다)")
+      }
+      .alert("저장 실패", isPresented: $showErrorAlert){
+        Button("확인", role: .destructive) { }
+      } message: {
+        Text("올바른 페이지 번호가 아닙니다.")
+      }
     }
+    .background(.backgroundDefault)
   }
-  
+  // 숫자 외에 텍스트 필터링 및 0 못오게
   private func formatDigits(_ input: String) -> String {
     let filtered = input.filter { $0.isNumber }
     if filtered.hasPrefix("0"), filtered.count >= 1 {
       return String(filtered.drop(while: { $0 == "0" }))
     }
     return filtered
-  }
-  
-  // MARK: (F)guideSection
-  @ViewBuilder
-  private func guideSection() -> some View {
-    VStack(alignment: .leading, spacing: 8) {
-      HStack {
-        Text("빵식이의 가이드")
-          .foregroundStyle(.black)
-          .brStyleFont(.pretendard(.semiBold, size: 18), lineHeight: 1.2)
-          .frame(maxWidth: .infinity, alignment: .leading)
-        
-        Image(systemName: "trash")
-          .resizable()
-          .aspectRatio(contentMode: .fit)
-          .frame(width: 16, height: 16)
-          .foregroundStyle(.gray2)
-      }
-      
-      HStack(spacing: 40) {
-        Image(.happyBread)
-          .resizable()
-          .aspectRatio(contentMode: .fit)
-          .frame(height: 100)
-        
-        Text(guideText)
-          .foregroundStyle(.gray5)
-          .brStyleFont(.pretendard(.regular, size: 14), lineHeight: 1.4, letterSpacing: -0.025)
-          .frame(maxWidth: .infinity, alignment: .leading)
-      }
-      .padding(.vertical, 25)
-      .padding(.horizontal, 37)
-      .frame(maxWidth: .infinity)
-      .background(.green1)
-      .clipShape(RoundedRectangle(cornerRadius: 8))
-    }.padding(.top, 24)
   }
   
   // MARK: (F)pageSection
@@ -116,11 +96,17 @@ struct MemoView: View {
         .brStyleFont(.pretendard(.semiBold, size: 18), lineHeight: 1.2)
       
       HStack {
-        RoundedTextField(type: .pages, placeholder: "0", text: $startPage, isValid: true)
+        RoundedTextField(type: .pages, placeholder: "0", text: $viewModel.startPage)
+          .onChange(of: viewModel.startPage) {
+            viewModel.startPage = formatDigits($1)
+          }
         
         Text("~")
         
-        RoundedTextField(type: .pages, placeholder: "0", text: $endPage, isValid: true)
+        RoundedTextField(type: .pages, placeholder: "0", text: $viewModel.endPage)
+          .onChange(of: viewModel.endPage) {
+            viewModel.endPage = formatDigits($1)
+          }
         
         Text("쪽")
       }.padding(.trailing, 52)
@@ -135,13 +121,107 @@ struct MemoView: View {
         .foregroundStyle(.black)
         .brStyleFont(.pretendard(.semiBold, size: 18), lineHeight: 1.2)
       
-     // TODO: 텍스트 에디터 들어오면 넣기
+      CustomTextEditor(
+        text: $viewModel.content,
+        isFocused: $textEditorFocused,
+        placeholder: "여기를 터치해서 문장을 입력할 수 있어요"
+      )
+      .padding(.bottom, 16)
+      .frame(height: 200)
+      .tint(.gray9)
+    }
+  }
+}
+
+
+// MARK: -(S)GuideSectionView
+private struct GuideSectionView: View {
+  
+  @ObservedObject var viewModel: MemoViewModel
+  @Binding var showGuideAlert: Bool
+  
+  private let suggestionText = """
+  여기를 터치해서
+  빵식이가 제안하는
+  질문을 볼 수 있어요
+  """
+  
+  var body: some View {
+    VStack(alignment: .leading, spacing: 8) {
+      HStack {
+        Text("빵식이의 가이드")
+          .foregroundStyle(.black)
+          .brStyleFont(.pretendard(.semiBold, size: 18), lineHeight: 1.2)
+          .frame(maxWidth: .infinity, alignment: .leading)
+        
+        if viewModel.guideStatus == .complete {
+          Image(systemName: SFSymbol.trash.name)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 16, height: 16)
+            .foregroundStyle(.gray2)
+            .onTapGesture {
+              showGuideAlert = true
+            }
+        }
+      }
+      
+      HStack(alignment: .center, spacing: 40) {
+        guideText()
+      }
+      .frame(maxWidth: .infinity)
+      .frame(height: 150)
+      .background(.green1)
+      .clipShape(RoundedRectangle(cornerRadius: 8))
+      .onTapGesture {
+        if viewModel.guideStatus == .empty {
+          viewModel.send(.generateGuides)
+        }
+      }
+    }
+    .padding(.top, 24)
+  }
+  
+  // MARK: (F)guideText
+  @ViewBuilder
+  private func guideText() -> some View {
+    switch viewModel.guideStatus {
+    case .loading:
+      ProgressView()
+      
+    case .empty:
+      Image(.happyBread)
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+        .frame(height: 100)
+        .frame(maxWidth: .infinity)
+        .padding(.leading, 37)
+      
+      Text(suggestionText)
+        .foregroundStyle(.gray5)
+        .brStyleFont(.pretendard(.regular, size: 14), lineHeight: 1.4, letterSpacing: -0.025)
+        .frame(maxWidth: .infinity, alignment: .leading)
+      
+    case .complete:
+      ScrollView {
+        VStack(alignment: .leading, spacing: 10) {
+          ForEach(viewModel.guides, id: \.self) { text in
+            HStack(alignment: .top, spacing: 4) {
+              Text("-")
+              Text(text)
+                .brStyleFont(.pretendard(.regular, size: 14), lineHeight: 1.2, letterSpacing: -0.025)
+            }
+          }
+        }.frame(maxWidth: .infinity, alignment: .leading)
+      }
+      .padding(.horizontal, 16)
+      .padding(.vertical, 12)
     }
   }
 }
 
 #Preview {
-  NavigationStack {
-    MemoView(targetDate: .now, totalPage: 200)
+  PreviewableContainer {
+    MemoView(viewModel: MemoViewModel(id: "exampleId", record: DummyData.dummyRecords.first!), totalPage: 300)
   }
 }
