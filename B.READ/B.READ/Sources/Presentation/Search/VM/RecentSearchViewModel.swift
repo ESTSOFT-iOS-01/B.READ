2902924534d1474345af947ff554f4c2cc7c6096
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-06-04 17:23:11 +0900

    [#129] Task Cancel 로직 추가

--- B.READ/B.READ/Sources/Presentation/Search/VM/RecentSearchViewModel.swift
+++ B.READ/B.READ/Sources/Presentation/Search/VM/RecentSearchViewModel.swift
@@ -12,9 +12,14 @@ final class RecentSearchViewModel: ObservableObject {
   
   // MARK: - State
   @Published var keywords: [String] = [] // 최근 검색어 리스트 - 검색창 활성화시에만 보여짐
+  private var currentTask: Task<Void, Never>? = nil
   
   // MARK: - Dependency
   @Dependency private var profileUseCase: ProfileUseCase
+  
+  deinit {
+    currentTask?.cancel()
+  }
   
   // MARK: - Action
   enum Action {
@@ -33,7 +38,7 @@ final class RecentSearchViewModel: ObservableObject {
       }
       
     case let .addKeyword(keyword),
-         let .selectKeyword(keyword):
+      let .selectKeyword(keyword):
       execute { [weak self] in
         try await self?.profileUseCase.addRecentKeyword(keyword)
         try await self?.fetch()
@@ -52,20 +57,30 @@ final class RecentSearchViewModel: ObservableObject {
       }
     }
   }
-  
+}
+
+private extension RecentSearchViewModel {
   private func fetch() async throws {
+    try Task.checkCancellation()
     let result = try await profileUseCase.fetchRecentKeywords()
+    try Task.checkCancellation()
     await MainActor.run {
       keywords = result
     }
   }
   
   private func execute(_ operation: @escaping () async throws -> Void) {
-    Task {
+    currentTask?.cancel()
+    currentTask = Task {
       do {
+        try Task.checkCancellation()
         try await operation()
       } catch {
-        print("Error: \(error)")
+        if Task.isCancelled {
+          print("작업 취소")
+        } else {
+          print("Error: \(error)")
+        }
       }
     }
   }
