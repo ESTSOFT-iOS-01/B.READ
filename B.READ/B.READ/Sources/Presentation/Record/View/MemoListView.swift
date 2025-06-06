e846bf373e3b7d93cd010088752a646598755e87
심근웅 <geunwoong.sim@gmail.com>
2025-06-07 02:30:29 +0900

    [#143] 메모 정렬 구현, 메모 셀 메뉴 구현중

--- B.READ/B.READ/Sources/Presentation/Record/View/MemoListView.swift
+++ B.READ/B.READ/Sources/Presentation/Record/View/MemoListView.swift
@@ -9,22 +9,25 @@ import SwiftUI
 
 // MARK: - (S)MemoListView
 struct MemoListView: View {
-  let memoGroups: [MemoGroup]
+  @EnvironmentObject private var coordinator: Coordinator<MainRoute, SheetRoute>
+  @ObservedObject var viewModel: RecordMemoViewModel
+  @State private var showMenuActionSheet = false
   
   var body: some View {
     ScrollView {
       LazyVStack(alignment: .leading, spacing: 8) {
         
-        ForEach(memoGroups) { group in
+        ForEach($viewModel.displayMemoGroups) { $group in
           Section {
-            ForEach(group.memos) { memo in
+            ForEach($group.memos) { $memo in
               MemoCell(
                 content: memo.content,
                 date: memo.createdAt,
                 startPage: memo.pages.0,
                 endPage: memo.pages.1
               ) {
-                
+                showMenuActionSheet = true
+                viewModel.selectedMemo = memo
               }
               .padding(.leading, 8)
             }
@@ -41,6 +44,30 @@ struct MemoListView: View {
       } // : LazyVStack
     } // : ScrollView
     .background(.backgroundDefault)
+    .confirmationDialog(
+      "메뉴를 선택하세요",
+      isPresented: $showMenuActionSheet,
+      titleVisibility: .hidden
+    ) {
+      menuActionSheet()
+    }
+  }
+  
+  // MARK: - (F)menuActionSheet
+  @ViewBuilder
+  private func menuActionSheet() -> some View {
+    Button("메모 수정") {
+//      guard let record = viewModel.record, let memo = viewModel.selectedMemo else { return }
+//      coordinator.push(.memo(id: memo.id, record: record))
+    }
+    
+    // TODO: - [시르] 삭제 alert띄우기
+    Button("메모 삭제", role: .destructive) {
+      guard let memo = viewModel.selectedMemo else { return }
+      viewModel.send(.deleteMemo(id: memo.id))
+    }
+    
+    Button("취소", role: .cancel) { }
   }
 }
 
--- B.READ/B.READ/Sources/Presentation/Record/ViewModel/RecordMemoViewModel.swift
+++ B.READ/B.READ/Sources/Presentation/Record/ViewModel/RecordMemoViewModel.swift
@@ -6,42 +6,43 @@
 //
 
 import Foundation
-// TODO: 순서대로 구현
-// 1. 검색어 submit하면 검색
-// 2. 검색어 하이라이트
-// 3. 실시간 검색어 반영(combine?)
 
 // MARK: - (C)RecordMemoViewModel
 final class RecordMemoViewModel: ObservableObject {
   
   // MARK: - State
-  struct RecordMemoViewState {
-    var displayMemoGroups: [MemoGroup] = []
-    var searchText: String = ""
-  }
-  
-  @Published var state: RecordMemoViewState = .init()
-  
+  @Published var displayMemoGroups: [MemoGroup] = []
+  @Published var searchText: String = ""
+  @Published var selectedSort: SortOption = .pageAscending
   
   // MARK: - Internal Variable
   private var memoGroups: [MemoGroup] = []
+  var selectedMemo: MemoVO? = nil
   
   // MARK: - Dependency
-//  @Dependency private var exampleUseCase: ExampleUseCase
+  @Dependency private var memoUseCase: MemoUseCase
   
   // MARK: - Action
   enum Action {
     case onAppear
+    case selectSort
     case onSubmit
+    case deleteMemo(id: String)
   }
   
   func send(_ action: Action) {
     switch action {
     case .onAppear:
-        loadMemoGroups()
-        sortDisplayMemoGroups()
+      loadMemoGroups()
+      
+    case .selectSort:
+      sortDisplayMemoGroups()
+      
     case .onSubmit:
-      print("검색어: \(state.searchText)")
+      print("검색어: \(searchText)")
+      
+    case .deleteMemo(let id):
+      deleteMemo(id: id)
     }
   }
 }
@@ -49,36 +50,77 @@ final class RecordMemoViewModel: ObservableObject {
 private extension RecordMemoViewModel {
   /// 메모를 불러와서 뷰에 보여줄 형태로 가공합니다.
   func loadMemoGroups() {
-//    // TODO: - memoUseCase.fetchAllMemo()으로 변경
-//    let allMemos = DummyData.dummyMemos
-//    
-//    let memoDict = Dictionary(grouping: allMemos, by: { $0.isbn })
-//    memoGroups = memoDict.compactMap { isbn, memos in
-//      
-//      let memoVOs = memos.map { MemoVO($0) }
-//      // TODO: - libraryUseCase.fetchBook(isbn)으로 변경
-//      // let book = await libraryUsecase.fetchBook(isbn)
-//      // return MemoGroup(isbn: isbn, bookTitle: book.name, memos: memoVOs)
-//      if let book = DummyData.dummyBooks.filter({ $0.isbn == isbn }).first {
-//        return MemoGroup(isbn: book.isbn, bookTitle: book.name, memos: memoVOs)
-//      } else {
-//        return nil
-//      }
-//    }
-    
-    // TODO: - 검색어 필터 들어가면 필터해주는 곳에서 처리해줌
-    state.displayMemoGroups = memoGroups
+    Task {
+      // 1. 전체 문장을 가져옴
+      guard let allMemos = try? await memoUseCase.fetchAllMemo() else {
+        print("RepositoryError.fetchError.errorDescription")
+        return
+      }
+      
+      // 2. 메모를 ISBN을 기준으로 나눔
+      let memoDict = Dictionary(grouping: allMemos, by: { $0.isbn })
+      
+      let memoGroups: [MemoGroup] = await withTaskGroup(of: MemoGroup?.self) {
+        [weak self] group in
+        guard let self = self else { return [] }
+        
+        // 3. 구분된 책의 문장을 QuoteGroup으로 생성
+        for (isbn, memos) in memoDict {
+          group.addTask {
+            do {
+              let bookTitle = try await self.memoUseCase.loadBookTitle(isbn)
+              let memoVOs = memos.map { MemoVO($0) }
+              return MemoGroup(isbn: isbn, bookTitle: bookTitle, memos: memoVOs)
+            } catch {
+              print("ERROR: MemoGroup Create Fail")
+              return nil
+            }
+          }
+        }
+        
+        var results: [MemoGroup] = []
+        
+        for await result in group {
+          if let item = result {
+            results.append(item)
+          }
+        }
+        
+        return results
+      } // : withTaskGroup
+      
+      // 4. 만들어진 MemoGroup을 반영
+      await MainActor.run {
+        self.memoGroups = memoGroups
+        // 5. MemoGroup 정렬을 진행
+        sortDisplayMemoGroups()
+      }
+    }
   }
   
-  // TODO: - 정렬 버트 만들고 기능 추가 예정
   /// 보여주고자 하는 Memo의 순서를 정렬합니다.
   func sortDisplayMemoGroups() {
-    // 1. MemoGroup을 정렬
-    // 2. MemoGroup에 담긴 Memos를 정렬
-    state.displayMemoGroups = state.displayMemoGroups.sorted { $0.bookTitle < $1.bookTitle }
+    let sortedGroup = memoGroups
+    // 1. 그룹 내부의 메모를 정렬
+      .map { group in
+        var sortedGroup = group
+        sortedGroup.memos = sortedGroup.memos.sorted(by: self.selectedSort.sort)
+        return sortedGroup
+      }
+    // 2. 그룹을 이름 순으로 정렬
+      .sorted(by: self.selectedSort.sort)
     
-    for (index, memoGroup) in state.displayMemoGroups.enumerated() {
-      state.displayMemoGroups[index].memos = memoGroup.memos.sorted { $0.pages.0 < $1.pages.0 }
+    self.displayMemoGroups = sortedGroup
+  }
+  
+  /// 선택된 메모를 삭제 합니다.
+  func deleteMemo(id: String) {
+    Task {
+      // 1. 데이터 삭제
+      try await memoUseCase.deleteMemo(id: id)
+      
+      // 2. 내부 데이터에 삭제를 반영
+      // 메모가 담긴 그룹을 찾아서 메모를 삭제 후 반영해야함
     }
   }
 }
