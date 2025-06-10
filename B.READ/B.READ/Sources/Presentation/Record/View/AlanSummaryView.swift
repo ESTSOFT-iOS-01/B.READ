d250a18c049fb3c4bcd618d145d2bef63551173a
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-06-10 10:55:13 +0900

    [#120] AlanSummaryView 더미 뷰 구현

--- B.READ/B.READ/Sources/Presentation/Record/View/AlanSummaryView.swift
+++ B.READ/B.READ/Sources/Presentation/Record/View/AlanSummaryView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct AlanSummaryView: View {
+  @StateObject var viewModel: SummaryViewModel
   @EnvironmentObject var coordinator: Coordinator<MainRoute, SheetRoute>
   
   var body: some View {
-    
-    
+    ScrollView {
+      VStack(alignment: .center, spacing: 12) {
+        InfoView(title: "🗓️ 독서 기간", content: "2025. 06. 06 ~ 2025. 06. 06")
+        
+        InfoView(title: "🏷️ 감정 태그", content: "")
+        
+        InfoView(title: "📚 요약", content: text)
+        
+        MultiInfoView(title: "🍞 문장", content: [""])
+        
+        MultiInfoView(title: "📝 메모", content: [""])
+      }
+    }
+    .navigationTitle(viewModel.record.title)
+    .background(.backgroundDefault, ignoresSafeAreaEdges: .all)
   }
 }
 
 #Preview {
   AlanSummaryView()
 }
+
