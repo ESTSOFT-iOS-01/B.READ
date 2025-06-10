b5c8ce599bf4de4a7b8927563d274a097c7fa3db
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-06-10 10:54:21 +0900

    [#120] MultiInfoView 구현

---
+++ B.READ/B.READ/Sources/Presentation/Common/Components/MultiInfoView.swift
@@ -0,0 +1,44 @@
+//
+//  MultiInfoView.swift
+//  B.READ
+//
+//  Created by 김도연 on 6/10/25.
+//
+
+import SwiftUI
+
+// MARK: - (S)MultiInfoView
+struct MultiInfoView: View {
+  let layoutPadding : CGFloat = 16
+  let title : String
+  var content: [String]
+  
+  var body: some View {
+    VStack(alignment: .leading, spacing: layoutPadding) {
+      Text(title)
+        .brStyleFont(.pretendard(.semiBold, size: 16), lineHeight: 1.2, letterSpacing: 0.02)
+        .frame(maxWidth: .infinity, alignment: .leading)
+      
+      VStack(alignment: .leading, spacing: 8) {
+        ForEach(content.indices, id: \.self) { index in
+          InnerContentView(content: content[index])
+        }
+      }
+    }
+    .padding(.horizontal, 24)
+    .padding(.vertical, 16)
+    .frame(maxWidth: .infinity, alignment: .leading)
+    .background(.white)
+  }
+}
+
+#Preview {
+  MultiInfoView(
+    title: "🍞 문장",
+    content: [
+      "Lorem ipsum dolor sit amet consectetur. Nec neque non sit nulla eli",
+      "Lorem ipsum dolor sit amet consectetur. Nec neque non sit nulla eli",
+      "Lorem ipsum dolor sit amet consectetur. Nec neque non sit nulla eli",
+      "Lorem ipsum dolor sit amet consectetur. Nec neque non sit nulla eli"
+    ])
+}
