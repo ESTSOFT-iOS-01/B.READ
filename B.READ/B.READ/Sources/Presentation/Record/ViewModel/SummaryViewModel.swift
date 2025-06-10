24782a6ad29dc180f7e317b9e383f8a2877d526d
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-06-10 16:28:10 +0900

    [#176] generateSummary 함수 구현

--- B.READ/B.READ/Sources/Presentation/Record/ViewModel/SummaryViewModel.swift
+++ B.READ/B.READ/Sources/Presentation/Record/ViewModel/SummaryViewModel.swift
@@ -8,14 +8,25 @@
 import Foundation
 import SwiftUI
 
+enum SummaryState {
+  case loading, loaded, failed
+}
+
 final class SummaryViewModel: ObservableObject {
-  let record: RecordDetailVO
-  let memos: [MemoVO]
-  let quotes: [QuoteVO]
+  // MARK: - Internal Variable
+  private let record: RecordDetailVO
+  private let memos: [MemoVO]
+  private let quotes: [QuoteVO]
   
-  @Published var summary: SummaryVO
+  // MARK: - State
+  @Published var dataState: SummaryState = .loading
+  @Published var summary: SummaryVO?
   @Published var memoData: [String] = []
   @Published var quoteData: [String] = []
+  
+  // MARK: - Dependency
+  @Dependency
+  private var summaryUseCase: SummaryUseCase
   
   init(record: RecordDetailVO, memos: [MemoVO], quotes: [QuoteVO]) {
     self.record = record
@@ -25,7 +36,45 @@ final class SummaryViewModel: ObservableObject {
     memoData = memos.map { $0.content }
     quoteData = quotes.map { $0.content }
     
-    summary = SummaryVO(DummyData.summaryForFetchTest)
+    // 요약 생성
+  }
+  
+  enum Action {
+    case onAppear
+    case generateSummary
+    case saveSummary
+  }
+  
+  func send(_ action: Action) {
+    switch action {
+    case .onAppear:
+    case .generateSummary:
+    case .saveSummary:
+    }
+  }
+  
+}
+
+// MARK: - Internal Function
+private extension SummaryViewModel {
+  func generateSummary(recordVO: RecordDetailVO) {
+    Task{
+      do {
+        let recordData = recordVO.toEntity(memos: memos, quotes: quotes)
+        let summaryData = try await summaryUseCase.generateSummary(in: recordData)
+        
+        await MainActor.run {
+          summary = SummaryVO(summaryData)
+          dataState = .loaded
+        }
+      } catch {
+        print(error)
+        await MainActor.run {
+          dataState = .failed
+        }
+      }
+      
+    }
   }
   
   
