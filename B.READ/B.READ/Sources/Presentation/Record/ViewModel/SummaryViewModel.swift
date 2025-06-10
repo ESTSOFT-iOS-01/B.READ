1b942af797061ceb10527eac2f00135707f89d86
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-06-10 11:27:19 +0900

    [#120] [VO] -> [String] 변환 함수 작성

--- B.READ/B.READ/Sources/Presentation/Record/ViewModel/SummaryViewModel.swift
+++ B.READ/B.READ/Sources/Presentation/Record/ViewModel/SummaryViewModel.swift
@@ -12,12 +12,31 @@ final class SummaryViewModel: ObservableObject {
   let record: RecordDetailVO
   let memos: [MemoVO]
   let quotes: [QuoteVO]
-  @Published var summary: SummaryVO?
+  @Published var summary: SummaryVO
+  @Published var memoData: [String] = []
+  @Published var quoteData: [String] = []
   
   init(record: RecordDetailVO, memos: [MemoVO], quotes: [QuoteVO]) {
     self.record = record
     self.memos = memos
     self.quotes = quotes
+    
+    memoData = memos.extractContents()
+    quoteData = quotes.extractContents()
+    
+    summary = SummaryVO(DummyData.summaryForFetchTest)
   }
   
+}
+
+extension Array where Element == MemoVO {
+  func extractContents() -> [String] {
+    self.map { $0.content }
+  }
+}
+
+extension Array where Element == QuoteVO {
+  func extractContents() -> [String] {
+    self.map { $0.content }
+  }
 }
